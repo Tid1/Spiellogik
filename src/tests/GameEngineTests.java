42aaf1a10f3d2b7dd14526d4c0ebd8f012a492d4
Tid1 <Cheikh.Mbaye@Student.HTW-Berlin.de>
2020-11-26 18:45:59 +0100

    Start testgetriebener Entwicklung

--- src/tests/GameEngineTests.java
+++ src/tests/GameEngineTests.java
@@ -1,6 +1,7 @@
 import Model.Exceptions.GameException;
 import Model.Exceptions.StatusException;
 import Model.Spiellogik.Figuren.Position;
+import Model.Spiellogik.Figuren.Typ;
 import Model.Spiellogik.Figuren.iPiece;
 import Model.Spiellogik.iBoard;
 import org.junit.jupiter.api.Test;
@@ -56,10 +57,10 @@ public class GameEngineTests {
     void bauerBlocked() throws GameException, StatusException {
         iBoard board = null;
         iPiece bauerWhite = null;
-        iPiece bauerBlack = null;
+        iPiece figur = null;
 
         bauerWhite.setPosition(4, 4);
-        bauerBlack.setPosition(5,4);
+        figur.setPosition(5,4);
 
         board.move(bauerWhite, 5, 4);
 
@@ -88,4 +89,66 @@ public class GameEngineTests {
 
     }
 
+    @Test
+    void bauerGeschlagenFehler() throws GameException, StatusException {
+        iBoard board = null;
+        iPiece bauerWhiteOne = null;
+        iPiece bauerWhiteTwo = null;
+
+        bauerWhiteOne.setPosition(4,4);
+        bauerWhiteTwo.setPosition(5,5);
+
+        board.move(bauerWhiteOne, 5,5);
+
+        Position expectedPosition = new Position(4,4);
+
+        assertEquals(bauerWhiteOne.getPosition().getX(), expectedPosition.getX());
+        assertEquals(bauerWhiteOne.getPosition().getY(), expectedPosition.getY());
+        assertFalse(bauerWhiteTwo.isCaptured());
+    }
+
+    @Test
+    void bauerZuKoenigen() throws GameException, StatusException {
+        iBoard board = null;
+        iPiece bauerWhite = null;
+
+        bauerWhite.setPosition(7,3);
+        board.move(bauerWhite, 8,3);
+
+        Position expectedPosition = new Position(8,3);
+        Typ typ = Typ.KOENIG;
+
+        assertEquals(bauerWhite.getPosition().getX(), expectedPosition.getX());
+        assertEquals(bauerWhite.getPosition().getY(), expectedPosition.getY());
+        assertEquals(bauerWhite.getType(), typ);
+    }
+
+    @Test
+    void bauerDoubleMoveFehler() throws GameException, StatusException {
+        iBoard board = null;
+        iPiece bauer = null;
+
+        bauer.setPosition(4, 5);
+        board.move(bauer, 6, 5);
+
+        Position expectedPosition = new Position(4, 5);
+
+        assertEquals(bauer.getPosition().getX(), expectedPosition.getX());
+        assertEquals(bauer.getPosition().getY(), expectedPosition.getY());
+    }
+
+    @Test
+    void pieceOutOfBounds() throws GameException, StatusException {
+        iBoard board = null;
+        iPiece piece = null;
+
+        piece.setPosition(7, 5);
+        board.move(piece, 8, 5);
+
+        Position expectedPosition = new Position(7, 5);
+
+        assertEquals(piece.getPosition().getX(), expectedPosition.getX());
+        assertEquals(piece.getPosition().getY(), expectedPosition.getY());
+
+    }
 }
