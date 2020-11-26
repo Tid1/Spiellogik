29a304cdd8188108b744c933203680eed0645328
Tid1 <Cheikh.Mbaye@Student.HTW-Berlin.de>
2020-11-26 12:12:57 +0100

    changeTurns private

--- src/Model/Spiellogik/BoardImpl.java
+++ src/Model/Spiellogik/BoardImpl.java
@@ -133,7 +133,6 @@ public class BoardImpl implements iBoard{
 
     }
 
-    @Override
     private void changeTurns() throws StatusException {
         if (this.status != Status.TURN_BLACK && this.status != Status.TURN_WHITE){
             throw new StatusException("Cant Swap turns");
