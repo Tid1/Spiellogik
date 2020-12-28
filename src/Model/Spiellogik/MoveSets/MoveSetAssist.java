c3e44a7b8648998fe654e5148cb3f2a30e5bcfa6
Joshua <joshua.harazim@hotmail.de>
2020-12-28 15:21:29 +0100

    MoveSetAssist getDiagonalMoveset Finish

--- src/Model/Spiellogik/MoveSets/MoveSetAssist.java
+++ src/Model/Spiellogik/MoveSets/MoveSetAssist.java
@@ -9,11 +9,68 @@ import java.util.List;
 public class MoveSetAssist {
     static List<Position> getDiagonalMoveset(BoardImpl board, Color picecolor, Position currentPosition) {
         List<Position> validMoves = new LinkedList<>();
-        for (int i = currentPosition.getX(); i < board.getUPPERBOUNDS(); i++){
-            validMoves.add(new Position(i, currentPosition.getY()));
-            validMoves.add(new Position(currentPosition.getX(), i));
+        boolean tr=true, tl=true, br=true, bl=true;
+        for (int i = 1; i<=board.getUPPERBOUNDS(); i++){
+            if (tr) {
+                if (currentPosition.getX()+i>board.getUPPERBOUNDS() || currentPosition.getY()+i>board.getUPPERBOUNDS()) {
+                    tr=false;
+                } else {
+                    if (board.onField(currentPosition.getX()+i, currentPosition.getY()+i) == null) {
+                        validMoves.add(new Position(currentPosition.getX()+i, currentPosition.getY()+i));
+                    } else if (board.onField(currentPosition.getX()+i, currentPosition.getY()+i).getColor() !=picecolor) {
+                        validMoves.add(new Position(currentPosition.getX()+i, currentPosition.getY()+i));
+                        tr=false;
+                    } else {
+                        tr=false;
+                    }
+                }
+            }
+            if (tl) {
+                if (currentPosition.getX()-i>board.getLOWERBOUNDS() || currentPosition.getY()+i>board.getUPPERBOUNDS()) {
+                    tl=false;
+                } else {
+                    if (board.onField(currentPosition.getX()-i, currentPosition.getY()+i) == null) {
+                        validMoves.add(new Position(currentPosition.getX()-i, currentPosition.getY()+i));
+                    } else if (board.onField(currentPosition.getX()-i, currentPosition.getY()+i).getColor() !=picecolor) {
+                        validMoves.add(new Position(currentPosition.getX()-i, currentPosition.getY()+i));
+                        tl=false;
+                    } else {
+                        tl=false;
+                    }
+                }
+            }
+            if (br) {
+                if (currentPosition.getX()+i>board.getUPPERBOUNDS() || currentPosition.getY()-i>board.getLOWERBOUNDS()) {
+                    br=false;
+                } else {
+                    if (board.onField(currentPosition.getX()+i, currentPosition.getY()-i) == null) {
+                        validMoves.add(new Position(currentPosition.getX()+i, currentPosition.getY()-i));
+                    } else if (board.onField(currentPosition.getX()+i, currentPosition.getY()-i).getColor() !=picecolor) {
+                        validMoves.add(new Position(currentPosition.getX()+i, currentPosition.getY()-i));
+                        br=false;
+                    } else {
+                        br=false;
+                    }
+                }
+            }
+            if (bl) {
+                if (currentPosition.getX()-i>board.getLOWERBOUNDS() || currentPosition.getY()-i>board.getLOWERBOUNDS()) {
+                    br=false;
+                } else {
+                    if (board.onField(currentPosition.getX()-i, currentPosition.getY()-i) == null) {
+                        validMoves.add(new Position(currentPosition.getX()-i, currentPosition.getY()-i));
+                    } else if (board.onField(currentPosition.getX()-i, currentPosition.getY()-i).getColor() !=picecolor) {
+                        validMoves.add(new Position(currentPosition.getX()-i, currentPosition.getY()-i));
+                        bl=false;
+                    } else {
+                        bl=false;
+                    }
+                }
+            }
+            if (!tr && !tl && !br && !bl) {
+                break;
+            }
         }
-
         return validMoves;
     }
 }
